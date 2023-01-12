53e3701411b4958d1dbdf2c57440f6d3398f59b0
Paata Home <paata.tepnadze@gmail.com>
2023-01-12 14:08:28 +0100

    create use state input value and use effect hook

--- react-redux-toolkit-rktquery/src/pages/HomePage.tsx
+++ react-redux-toolkit-rktquery/src/pages/HomePage.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchUsersQuery } from '../store/github/github.api';
 
 export const HomePage = () => {
+  const [search, setSearch] = useState('');
   const { isLoading, isError, data } = useSearchUsersQuery('vsvs');
-  console.log(data);
+
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
+
+  //console.log(data);
 
   return (
     <div className="flex  justify-center pt-10 mx-auto h-screen w-screen ">
@@ -15,6 +21,8 @@ export const HomePage = () => {
         <input
           className="border py-2 px-4 w-full h-[40px] mb-2 "
           placeholder="Search for github username..."
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
         />
         <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
