1ce87263345e7a6b86c5d583292f7b30255a9bc3
Paata Home <paata.tepnadze@gmail.com>
2023-01-12 14:02:21 +0100

    add searchbar input and dropdown styles

--- react-redux-toolkit-rktquery/src/pages/HomePage.tsx
+++ react-redux-toolkit-rktquery/src/pages/HomePage.tsx
@@ -5,7 +5,24 @@ export const HomePage = () => {
   const { isLoading, isError, data } = useSearchUsersQuery('vsvs');
   console.log(data);
 
-  return <div>HomePage</div>;
+  return (
+    <div className="flex  justify-center pt-10 mx-auto h-screen w-screen ">
+      {isError && (
+        <p className="text-center text-red-600">something went wrong...</p>
+      )}
+
+      <div className=" relative w-[500px]  ">
+        <input
+          className="border py-2 px-4 w-full h-[40px] mb-2 "
+          placeholder="Search for github username..."
+        />
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+          accusantium?
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
