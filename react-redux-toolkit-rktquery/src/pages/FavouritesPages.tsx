6672a679bd97608e67aa1952bbd02ef2db902a9d
Paata Home <paata.tepnadze@gmail.com>
2023-01-13 12:38:12 +0100

    create favourite pages tsx

--- react-redux-toolkit-rktquery/src/pages/FavouritesPages.tsx
+++ react-redux-toolkit-rktquery/src/pages/FavouritesPages.tsx
@@ -1,7 +1,23 @@
+import { stat } from 'fs';
 import React from 'react';
+import { useAppSelector } from '../hooks/redux';
 
 const FavouritesPages = () => {
-  return <div>FavouritesPages</div>;
+  const { favourites } = useAppSelector((state) => state.github);
+
+  if (favourites.length === 0) return <p className="text-center">No items</p>;
+
+  return (
+    <ul className="list-none  ">
+      {favourites.map((f) => (
+        <li className="" key={f}>
+          <a href={f} target="_blank">
+            {f}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default FavouritesPages;
